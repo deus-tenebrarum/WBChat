1838d54906eea07d7d690bc605165858c4d4167c
deus_tenebrarum <dromanov087@gmail.com>
2025-11-16 19:11:17 +0300

    RegLoginAlpha2

--- WBChat/accounts/models.py
+++ WBChat/accounts/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 class User(AbstractUser):
     department = models.CharField(max_length=100, blank=True)  # отдел
-    avatar = models.ImageField(upload_to='avatars', blank=True, null=True)
+    avatar = models.ImageField(upload_to='', blank=True, null=True)
 
     def __str__(self):
         return self.username
