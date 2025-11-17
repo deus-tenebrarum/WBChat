5d59e260fead6ff6e1e59c754cc312853088700f
CriticalKawaii <94896777+CriticalKawaii@users.noreply.github.com>
2025-11-17 23:27:55 +0300

    initial commit

---
+++ WBChat/accounts/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    department = models.CharField(max_length=100, blank=True)  # отдел
+    avatar = models.ImageField(upload_to='', blank=True, null=True)
+
+    def __str__(self):
+        return self.username
---
+++ WBChat/accounts/views.py
@@ -0,0 +1,38 @@
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from .forms import RegisterForm
+from django.contrib.auth.decorators import login_required
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('login')
+    else:
+        form = RegisterForm()
+
+    return render(request, 'accounts/register.html', {'form': form})
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('profile')  # куда ведём после логина
+        else:
+            messages.error(request, "Неверный логин или пароль")
+
+    return render(request, 'accounts/login.html')
+@login_required
+def profile_view(request):
+    return render(request, 'accounts/profile.html', {'user': request.user})
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('login')
